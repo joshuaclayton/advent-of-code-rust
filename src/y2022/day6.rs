104c9627994d0a6d2b9e78f5aa4cc09537f98f23
Josh Clayton <joshua.clayton@gmail.com>
2022-12-06 07:43:13 -0500

    2022 Day 6 part 2

--- src/y2022/day6.rs
+++ src/y2022/day6.rs
@@ -1,7 +1,5 @@
 use std::collections::HashSet;
 
-use itertools::Itertools;
-
 pub fn solve() {
     let input = include_str!("input-day6");
     println!("Answer: {:?}", run(input));
@@ -10,11 +8,11 @@ pub fn solve() {
 fn run(input: &str) -> usize {
     let mut final_position = 0;
 
-    for (idx, (first, second, third, fourth)) in input.chars().tuple_windows().enumerate() {
-        let result: HashSet<char> = vec![first, second, third, fourth].into_iter().collect();
+    for (idx, full_window) in input.as_bytes().windows(14).enumerate() {
+        let result: HashSet<_> = full_window.into_iter().collect();
 
-        if result.len() == 4 {
-            final_position = idx + 4;
+        if result.len() == 14 {
+            final_position = idx + 14;
             break;
         }
     }
@@ -25,9 +23,9 @@ fn run(input: &str) -> usize {
 mod tests {
     #[test]
     fn solve_returns_the_correct_value() {
-        assert_eq!(super::run("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
-        assert_eq!(super::run("nppdvjthqldpwncqszvftbrmjlhg"), 6);
-        assert_eq!(super::run("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
-        assert_eq!(super::run("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+        assert_eq!(super::run("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+        assert_eq!(super::run("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+        assert_eq!(super::run("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+        assert_eq!(super::run("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
     }
 }
