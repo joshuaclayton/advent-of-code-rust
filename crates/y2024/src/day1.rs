ef91d2044affda9baa4b101f6408e2bfdd3377bb
Josh Clayton <joshua.clayton@gmail.com>
2024-12-12 07:22:11 -0500

    2024, Day 1 part 1

--- crates/y2024/src/day1.rs
+++ crates/y2024/src/day1.rs
@@ -5,31 +5,42 @@ use nom::{
     sequence::terminated,
     IResult,
 };
+use std::collections::HashMap;
 
 pub fn run() -> anyhow::Result<()> {
     let input = include_str!("input-day1");
     let (_, pairs) = parse_input(input)?;
 
-    let mut lefts = pairs.iter().map(|p| p.0).collect::<Vec<_>>();
-    let mut rights = pairs.iter().map(|p| p.1).collect::<Vec<_>>();
+    let lefts = pairs.iter().map(|p| p.0).collect::<Vec<_>>();
+    let rights = tally(pairs.iter().map(|p| p.1));
 
-    lefts.sort();
-    rights.sort();
+    let mut similarity_score = 0;
 
-    let mut distance = 0;
-
-    lefts.iter().zip(rights).for_each(|(l, r)| {
-        distance += (r - l).abs();
-        println!("{} {}", l, r);
+    lefts.iter().for_each(|l| {
+        similarity_score += (*rights.get(l).unwrap_or(&0) as isize) * *l;
     });
 
-    println!("distance: {}", distance);
+    println!("similarity score: {}", similarity_score);
 
     Ok(())
 }
 
 #[derive(Debug, Clone)]
 struct Pair(isize, isize);
+
+fn tally<T, I>(iterable: I) -> HashMap<T, usize>
+where
+    T: Eq + std::hash::Hash,
+    I: IntoIterator<Item = T>,
+{
+    let mut tally = HashMap::new();
+
+    for value in iterable {
+        *tally.entry(value).or_insert(0) += 1;
+    }
+
+    tally
+}
 
 fn parse_input(input: &str) -> IResult<&str, Vec<Pair>> {
     all_consuming(terminated(
